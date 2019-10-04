2fc51be33188af62ceeeeea3f47022cd3d3b28b7
Achraf Faddoul <faddoulachraf39@gmail.com>
2019-10-04 17:20:39 +0100

    create ft_edgenew

--- srcs/ft_edgenew.c
+++ srcs/ft_edgenew.c
@@ -6,13 +6,13 @@
 /*   By: afaddoul <afaddoul@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/04 17:01:50 by afaddoul          #+#    #+#             */
-/*   Updated: 2019/10/04 17:04:34 by afaddoul         ###   ########.fr       */
+/*   Updated: 2019/10/04 17:20:00 by afaddoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-t_edges			*ft_roomnew(char **links)
+t_edges			*ft_edgenew(char **links)
 {
 	t_egdes		*edge;
 
@@ -21,5 +21,6 @@ t_edges			*ft_roomnew(char **links)
 		return (NULL);
 	edge->vertex = links[0];
 	edge->neighbor = links[1];
+	ft_memdel((void**)links);
 	return (edge);
 }
