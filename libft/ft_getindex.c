fccf430d9e3d602220ba77b98b9842e521d97b62
Achraf Faddoul <faddoulachraf39@gmail.com>
2019-10-07 05:35:18 +0100

    ft_getindex added to libft

---
+++ libft/ft_getindex.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_getindex.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afaddoul <afaddoul@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/07 05:22:48 by afaddoul          #+#    #+#             */
+/*   Updated: 2019/10/07 05:27:50 by afaddoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t		ft_getindex(t_hashtable *ht, const char key)
+{
+	size_t 	index;
+
+	index = compute_hash(key) % ht->size;
+	return (index);
+}
