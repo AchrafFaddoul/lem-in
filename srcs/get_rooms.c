8d72d3e11dbcee8b3d533354ea75aa65cac6e127
afaddoul <faddoulachraf39@gmail.com>
2020-01-12 15:58:20 +0100

    get_edges optimisation

--- srcs/get_rooms.c
+++ srcs/get_rooms.c
@@ -6,13 +6,13 @@
 /*   By: afaddoul <afaddoul@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 14:12:36 by afaddoul          #+#    #+#             */
-/*   Updated: 2020/01/12 12:59:11 by afaddoul         ###   ########.fr       */
+/*   Updated: 2020/01/12 15:57:29 by afaddoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-static int		get_type(const char *line)
+static int			get_type(const char *line)
 {
 	if (*line == 'L')
 		return (T_INSTRUCTION);
@@ -25,11 +25,41 @@ static int		get_type(const char *line)
 	return (T_VERTEX);
 }
 
-t_element		*ft_getrooms(t_farm *farm, t_element *elm)
+static t_element	*ft_inputskip(t_farm *farm, t_element *current,
+		t_room *room, int ret)
+{	
+	if (ret == T_START)
+	{
+		if (farm->start)
+			return (NULL);
+		if (!current->next)
+			return (NULL);
+		current = current->next;
+		if (!(room = ft_roomparse((char*)(current->content))))
+			return (NULL);
+		farm->start = room;
+		ft_dlstpush(farm->rooms, ft_elemnew(room));
+	}
+	else if (ret == T_END)
+	{
+		if (farm->end)
+			return (NULL);
+		if (!current->next)
+			return (NULL);
+		current = current->next;
+		if (!(room = ft_roomparse((char*)(current->content))))
+			return (NULL);
+		farm->end = room;
+		ft_dlstpush(farm->rooms, ft_elemnew(room));
+	}
+	return (current);
+}
+
+t_element			*ft_getrooms(t_farm *farm, t_element *elm)
 {
-	t_element	*current;
-	t_room		*room;
-	int			ret;
+	t_element		*current;
+	t_room			*room;
+	int				ret;
 
 	current = elm;
 	farm->rooms = ft_dlstnew();
@@ -42,30 +72,8 @@ t_element		*ft_getrooms(t_farm *farm, t_element *elm)
 			current = current->next;
 			continue ;
 		}
-		else if (ret == T_START)
-		{
-			if (farm->start)
-				return (NULL);
-			if (!current->next)
-				return (NULL);
-			current = current->next;
-			if (!(room = ft_roomparse((char*)(current->content))))
-				return (NULL);
-			farm->start = room;
-			ft_dlstpush(farm->rooms, ft_elemnew(room));
-		}
-		else if (ret == T_END)
-		{
-			if (farm->end)
-				return (NULL);
-			if (!current->next)
-				return (NULL);
-			current = current->next;
-			if (!(room = ft_roomparse((char*)(current->content))))
-				return (NULL);
-			farm->end = room;
-			ft_dlstpush(farm->rooms, ft_elemnew(room));
-		}
+		else if (!(current = ft_inputskip(farm, current, room, ret)))
+			return (NULL);
 		else if (!(room = ft_roomparse((char*)(current->content))))
 		{
 			if (ft_countof(current->content, '-') == 1)
